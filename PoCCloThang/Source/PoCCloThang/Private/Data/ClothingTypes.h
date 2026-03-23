a3fe2011a47145c4982275139f66cf8fe11d1785
Jeff Fattic <jfattic@live.com>
2026-03-23 14:57:19 -0500

    added outfitselectionwidget and clothingtypes

--- PoCCloThang/Source/PoCCloThang/Private/Data/ClothingTypes.h
+++ PoCCloThang/Source/PoCCloThang/Private/Data/ClothingTypes.h
@@ -1,15 +1,34 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ClothingTypes.generated.h"
 
-/**
- * 
- */
-class ClothingTypes
+UENUM(BlueprintType)
+enum class EClothingSlot : uint8
 {
-public:
-	ClothingTypes();
-	~ClothingTypes();
+    Jacket,
+    Top,
+    Bottom,
+    Shoes
+};
+
+USTRUCT(BlueprintType)
+struct FClothingItem : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    FName ItemID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    EClothingSlot Slot;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    FText DisplayName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TSoftObjectPtr<UTexture2D> Thumbnail;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TSoftObjectPtr<USkeletalMesh> Mesh;
 };
