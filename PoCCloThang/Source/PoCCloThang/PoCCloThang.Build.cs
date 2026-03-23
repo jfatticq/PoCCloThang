128e5ab5f061b66100aad7abe9ca3f1dc6b438a9
Jeff Fattic <jfattic@live.com>
2026-03-23 13:37:54 -0500

    initial branch setup

--- PoCCloThang/Source/PoCCloThang/PoCCloThang.Build.cs
+++ PoCCloThang/Source/PoCCloThang/PoCCloThang.Build.cs
@@ -24,20 +24,7 @@ public class PoCCloThang : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		PublicIncludePaths.AddRange(new string[] {
-			"PoCCloThang",
-			"PoCCloThang/Variant_Platforming",
-			"PoCCloThang/Variant_Platforming/Animation",
-			"PoCCloThang/Variant_Combat",
-			"PoCCloThang/Variant_Combat/AI",
-			"PoCCloThang/Variant_Combat/Animation",
-			"PoCCloThang/Variant_Combat/Gameplay",
-			"PoCCloThang/Variant_Combat/Interfaces",
-			"PoCCloThang/Variant_Combat/UI",
-			"PoCCloThang/Variant_SideScrolling",
-			"PoCCloThang/Variant_SideScrolling/AI",
-			"PoCCloThang/Variant_SideScrolling/Gameplay",
-			"PoCCloThang/Variant_SideScrolling/Interfaces",
-			"PoCCloThang/Variant_SideScrolling/UI"
+			"PoCCloThang"
 		});
 
 		// Uncomment if you are using Slate UI
